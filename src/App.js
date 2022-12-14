27997aa0bca65aaac78e3562dae3a2888517a5bf
Mansurbek-Eshbotayev <mustafoimron1820@gmail.com>
2022-12-14 21:36:30 +0500

    Add All file

--- src/App.js
+++ src/App.js
@@ -2,59 +2,65 @@ import { useState } from 'react';
 import './App.scss';
 import {List} from "./components/List/List"
 import {Listitem} from "./components/Listitem/Listitem"
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 
 function App() {
   const [value, setValue] = useState("")
   const [ todo, setTodo ] = useState(JSON.parse(window.localStorage.getItem("todo")) || [])
-
-function func(evt){
-  evt.preventDefault();
-  if(value.length){
-    const newTodo = (
-      {
-        id:todo.length ? todo[todo.length -1].id + 1 : 1,
-        text:value,
-        isComplate:false
-      }
-  )
-  setTodo([...todo,newTodo])
-  setValue("")
-  }
-}
-
-  const hendDel = (delId) =>{
-    const filterTodo = todo.filter(e => e.id !== delId)
-    setTodo([...filterTodo]);
-  }
-
-  const henEdit = (delId) =>{
-    const newText = prompt("Yangi so'z kiriting");
-    const findTodo = todo.find(e => e.id === delId);
-    findTodo.text = newText
-    setTodo([...todo])
-  }
-
-  const henChak = (delId) =>{
-    const findTodo = todo.find(e => e.id === delId);
-    findTodo.isComplate = !findTodo.isComplate
-    setTodo([...todo])
-  }
-
-  window.localStorage.setItem("todo", JSON.stringify(todo))
- 
-  return (
-    <div className="App">
-       <form onSubmit={func} className='mt-4 w-25 mx-auto d-flex'>
-         <input onChange={(evt) => setValue(evt.target.value)} value={value} className='list_input form-control' type="text" name='todo' placeholder='todo list...'/>
-         <button type='submit' className='btn btn-primary ms-3'>Add</button>
-         </form>
-
-      <List>
-        <Listitem>
+  
+  function func(evt){
+    evt.preventDefault();
+    if(value.length){
+      const newTodo = (
         {
-          todo.map(e => (
-            <div key={e.id} className='d-flex align-items-center justify-content-between wrapper'>
-            <span>{e.id}</span>
+          id:todo.length ? todo[todo.length -1].id + 1 : 1,
+          text:value,
+          isComplate:false
+        }
+        )
+        setTodo([...todo,newTodo])
+        setValue("")
+      }
+    }
+    
+    const hendDel = (delId) =>{
+      const filterTodo = todo.filter(e => e.id !== delId)
+      setTodo([...filterTodo]);
+      toast.error("Todo O'chirildi!");
+    }
+    
+    const henEdit = (delId) =>{
+      const newText = prompt("Yangi so'z kiriting");
+      const findTodo = todo.find(e => e.id === delId);
+      findTodo.text = newText
+      setTodo([...todo])
+      toast.warning("Todo o'zgardi!");
+    }
+    
+    const henChak = (delId) =>{
+      const findTodo = todo.find(e => e.id === delId);
+      findTodo.isComplate = !findTodo.isComplate
+      setTodo([...todo])
+      toast.success("Todo bajarildi!");
+    }
+    
+    window.localStorage.setItem("todo", JSON.stringify(todo))
+    
+    return (
+      <div className="App">
+      <form onSubmit={func} className='mt-4 w-25 mx-auto d-flex'>
+      <input onChange={(evt) => setValue(evt.target.value)} value={value} className='list_input form-control' type="text" name='todo' placeholder='todo list...'/>
+      <button type='submit' className='btn btn-primary ms-3'>Add</button>
+      </form>
+      
+      <List>
+      <Listitem>
+      {
+        todo.map(e => (
+          <div key={e.id} className='d-flex align-items-center justify-content-between wrapper'>
+          <span>{e.id}</span>
           <input onChange={()=>henChak(e.id)} defaultChecked={e.isComplate} type="checkbox"/>
           <p>{e.text}</p>
           <span>
@@ -65,9 +71,21 @@ function func(evt){
           ))
         }
         </Listitem>
-      </List>
-    </div>
-  );
-}
-
-export default App;
+        </List>
+        <ToastContainer 
+        position="bottom-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="dark"/>
+        </div>
+        );
+      }
+      
+      export default App;
+      
